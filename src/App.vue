0aa9e8ab8cd1e207606d60519fe8d906656d3376
siruislybro <tansirui2@gmail.com>
2024-03-06 13:17:08 +0800

    feat: implement login functionality

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import './firebaseConfig.js';
 
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <div class="nav">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
   </header>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class = "login-view">
+    <Header />
+    <div class = "content">
+      <LoginForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import LoginForm from "@/components/LoginForm.vue"
+export default {
+  name: "LoginView",
+  components: {
+    Header,
+    LoginForm,
+  },
+}
+</script>
+
+<style scoped>
+.login-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.content {
+  margin-left: 2rem;
+}
+</style>
