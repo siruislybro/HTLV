1a46cd39c460f077aae4fa0647b9dccadc379abe
Marcelleejx <marcelleejx@u.nus.edu>
2024-03-17 17:35:20 +0800

    Created Frontend of Itineraries view and AddLocationForm

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LoginView from '../views/LoginView.vue';
-import HomeView from '../views/HomeView.vue';
-import SignUpView from '../views/SignUpView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import HomeView from "../views/HomeView.vue";
+import SignUpView from "../views/SignUpView.vue";
 import ForgotPasswordView from "../views/ForgotPasswordView.vue";
+import ItinerariesView from "../views/ItinerariesView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: HomeView
+      path: "/",
+      name: "Home",
+      component: HomeView,
     },
     {
-      path: '/signup',
-      name: 'SignUp',
-      component: SignUpView
+      path: "/signup",
+      name: "SignUp",
+      component: SignUpView,
     },
     {
-      path: '/login',
-      name: 'Login',
-      component: LoginView
+      path: "/login",
+      name: "Login",
+      component: LoginView,
     },
     {
-      path: '/forgot-password',
-      name: 'ForgetPassword',
-      component: ForgotPasswordView
-    }
-  ]
-})
+      path: "/forgot-password",
+      name: "ForgetPassword",
+      component: ForgotPasswordView,
+    },
+    {
+      path: "/itineraries",
+      name: "Itineraries",
+      component: ItinerariesView,
+    },
+  ],
+});
 
-export default router
+export default router;
