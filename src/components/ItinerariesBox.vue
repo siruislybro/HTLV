2ceca33d70b3e0990ce34dfc9afbe769dde674b8
vvivianfang <133471507+vvivianfang@users.noreply.github.com>
2024-03-23 07:25:10 +0800

    update community with component

---
+++ src/components/ItinerariesBox.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="itinerary-details">
+    <img class="itinerary-pic" :src="itineraryPic" :alt="title" />
+    <h3>{{ title }}</h3>
+    <div class="profile-info">
+      <img class="profile-img" :src="profilePic" :alt="name" />
+      <p class="profile-name">{{ name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    profilePic: String,
+    name: String,
+    title: String,
+    itineraryPic: String,
+    selected: Boolean
+  },
+  computed: {
+    isSelected() {
+      return this.selected;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.itinerary-details {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 20px;
+  margin-bottom: 20px;
+  background-color: #ffcbcb;
+  width: 300px;
+  margin-left: 100px;
+}
+
+.profile-info {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.profile-img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+
+.profile-name {
+  font-weight: bold;
+  margin: 0;
+}
+
+.itinerary-pic {
+  width: 100%;
+  height: auto;
+  border-radius: 8px;
+}
+</style>
--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -1,119 +1,98 @@
 <template>
   <div class="community-view">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <NavBar />
     <div class="content">
-        <div class="trending-info">
-            <h2>Trending Placelists in your city</h2>
-            <p>See where others love to go!</p>
-        </div>
-        <div class="search-bar">
-            <select v-model="selectedLocation">
-            <option value="">Select Location</option>
-            <option value="Singapore">Singapore</option>
-            <option value="Japan">Japan</option>
-            <option value="Malaysia">Malaysia</option>
-            </select>
-            <button @click="search">Search</button>
-        </div>
-
-        <div class="results">
-        <div v-if="selectedLocation === 'Singapore'" class="country-info">
-          <div class="colored-box singapore">
-            <img class="mainpic" src="../assets/community_singapore1.jpeg" alt="Marina" />
-            <h3>City life</h3>
-            <div class="profile-info">
-                <img class="profile-img" src="../assets/community_singapore2.jpeg" alt="Matcha Overload" />
-                <p class="profile-name">Darrell</p>
-                <div class="upvote-downvote">
-                    <button @click="upvote(selectedLocation)">
-                        <i class="fas fa-arrow-up"></i>
-                    </button>
-                    <span>{{ upvotes['Singapore'] }}</span>
-                    <button @click="downvote(selectedLocation)">
-                        <i class="fas fa-arrow-down"></i>
-                    </button>
-                </div>
-            </div>
-          </div>
-        </div>
-        <div v-if="selectedLocation === 'Japan'" class="country-info">
-          <div class="colored-box japan">
-            <img class="mainpic" src="../assets/community_japan2.jpeg" alt="Matcha Girl" />
-            <h3>Matcha Overload</h3>
-            <div class="profile-info">
-                <img class="profile-img" src="../assets/community_japan1.jpeg" alt="Matcha Overload" />
-                <p class="profile-name">Vivian</p>
-                <div class="upvote-downvote">
-                    <button @click="upvote(selectedLocation)">
-                        <i class="fas fa-arrow-up"></i>
-                    </button>
-                    <span>{{ upvotes['Japan'] }}</span>
-                    <button @click="downvote(selectedLocation)">
-                        <i class="fas fa-arrow-down"></i>
-                    </button>
-                </div>
-            </div>
-          </div>
-        </div>
-        <div v-if="selectedLocation === 'Malaysia'" class="country-info">
-            <div class="colored-box malaysia">
-            <img class="mainpic" src="../assets/community_malaysia2.webp" alt="Marina" />
-            <h3>Malaysia Guide</h3>
-            <div class="profile-info">
-                <img class="profile-img" src="../assets/community_malaysia1.jpeg" alt="Matcha Overload" />
-                <p class="profile-name">Marcel</p>
-                <div class="upvote-downvote">
-                    <button @click="upvote(selectedLocation)">
-                        <i class="fas fa-arrow-up"></i>
-                    </button>
-                    <span>{{ upvotes['Malaysia'] }}</span>
-                    <button @click="downvote(selectedLocation)">
-                        <i class="fas fa-arrow-down"></i>
-                    </button>
-                </div>
-            </div>
-            </div>
-        </div>
+      <div class="trending-info">
+        <h2>Trending Placelists in your city</h2>
+        <p>See where others love to go!</p>
       </div>
+      <div class="search-bar">
+        <input type="text" v-model="searchQuery" placeholder="Search...">
+        <button @click="search">Search</button>
+      </div>
+
+      <div v-if="selectedLocation === 'Singapore'">
+        <ItinerariesBox
+          profilePic="../assets/community_singapore2.jpeg"
+          name="Darrell"
+          title="City life"
+          itineraryPic="../assets/community_singapore1.jpeg"
+        />
+      </div>
+
+      <div v-if="selectedLocation === 'Malaysia'">
+        <ItinerariesBox
+          profilePic="../assets/community_malaysia2.jpeg"
+          name="Marcel"
+          title="Nature adventures"
+          itineraryPic="../assets/community_malaysia1.jpeg"
+        />
+      </div>
+
+      <div v-if="selectedLocation === 'Japan'">
+        <ItinerariesBox
+          profilePic="../assets/community_japan2.jpeg"
+          name="Vivian"
+          title="Cultural experience"
+          itineraryPic="../assets/community_japan1.jpeg"
+        />
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import ItinerariesBox from "../components/ItinerariesBox.vue";
 
 export default {
   name: "CommunityView",
   components: {
     NavBar,
+    ItinerariesBox
   },
   data() {
     return {
-      selectedLocation: '',
-      upvotes: {
-        'Singapore': 0,
-        'Japan': 0,
-        'Malaysia': 0
-      }
+      searchQuery: "",
+      selectedLocation: null,
+      itineraries: [
+        {
+          location: "Singapore",
+          profilePic: "../assets/community_singapore2.jpeg",
+          name: "Darrell",
+          title: "City life",
+          itineraryPic: "../assets/community_singapore1.jpeg"
+        },
+        {
+          location: "Malaysia",
+          profilePic: "../assets/community_malaysia2.jpeg",
+          name: "Marcel",
+          title: "Nature adventures",
+          itineraryPic: "../assets/community_malaysia1.jpeg"
+        },
+        {
+          location: "Japan",
+          profilePic: "../assets/community_japan2.jpeg",
+          name: "Vivian",
+          title: "Matcha Overload",
+          itineraryPic: "../assets/community_japan1.jpeg"
+        }
+      ]
     };
   },
   methods: {
     search() {
-      // Implement search functionality here
-      // You can fetch search results from an API or perform client-side filtering
-      // Use this.selectedLocation to get the selected location
+    const normalizedQuery = this.searchQuery.toLowerCase();
+    const found = this.itineraries.find(item => item.location.toLowerCase() === normalizedQuery);
+    this.selectedLocation = found ? found.location : null;
     },
     upvote(location) {
-      // Increment upvotes count for the given location
-      this.upvotes[location]++;
+      console.log(`Upvoted ${location}`);
     },
     downvote(location) {
-      // Decrement upvotes count for the given location
-      // Add logic to prevent negative votes if needed
-      if (this.upvotes[location] > 0) {
-        this.upvotes[location]--;
-    }
+      console.log(`Downvoted ${location}`);
     }
   }
 };
@@ -129,7 +108,7 @@ export default {
 .content {
   text-align: left;
   margin-bottom: 2rem;
-  width: 100%; 
+  width: 100%;
 }
 
 .trending-info {
@@ -139,6 +118,7 @@ export default {
 
 .search-bar {
   margin-bottom: 20px;
+  margin-left: 100px;
 }
 
 .search-bar select {
@@ -165,82 +145,5 @@ export default {
 
 .country-info {
   margin-top: 20px;
-}
-
-.colored-box {
-  padding: 20px;
-  border-radius: 8px;
-  margin-right: 20px;
-  width: 300px;
-  position: relative; /* Position the upvote-downvote div relative to this */
-  overflow: hidden; /* Prevent upvote-downvote div from overflowing */
-  /* Added styles */
-  position: relative;
-}
-
-.singapore {
-  background-color: rgb(240, 167, 167);
-}
-
-.japan {
-  background-color: rgb(240, 167, 167);
-}
-
-.malaysia {
-  background-color: rgb(240, 167, 167);
-}
-
-.colored-box h3 {
-  margin-bottom: 10px;
-  color: black;
-}
-
-.colored-box p {
-  color: black;
-}
-
-.profile-info {
-  display: flex;
-  align-items: center;
-}
-
-.profile-img {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
-
-.profile-name {
-  margin-bottom: 100;
-  margin-left: 10px;
-}
-
-.mainpic {
-  width: 100%;
-  height: 200px;
-  object-fit: cover;
-}
-
-.upvote-downvote {
-  position: absolute;
-  bottom: 7%;
-  right: 0;
-  display: flex;
-  flex-direction: column;
-}
-
-.upvote-downvote button {
-  background: none;
-  border: none;
-  cursor: pointer;
-}
-
-.upvote-downvote button i {
-  color: #28a745;
-}
-
-.upvote-downvote button i.fa-arrow-down {
-  margin-top: 5px;
-  color: #dc3545;
 }
 </style>
