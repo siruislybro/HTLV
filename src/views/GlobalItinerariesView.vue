8181db20158266fd9db0f3c7c18afb552ae00997
vvivianfang <133471507+vvivianfang@users.noreply.github.com>
2024-04-20 16:06:43 +0800

    Global Components created
    PlacedToVisitGlobal and GlobalItinerariesView created

---
+++ src/views/GlobalItinerariesView.vue
@@ -0,0 +1,57 @@
+<template>
+  <NavBar /> 
+  <div class = "content-container"> 
+    <div class = "places">
+      <PlacesToVisitGlobal @place-selected="handlePlaceSelection" :itineraryId="itineraryId"/>
+    </div>
+    <div class = "maps">
+      <GoogleMaps :selectedPlace= "selectedPlace"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue"
+import GoogleMaps from "../components/GoogleMaps.vue"
+import PlacesToVisitGlobal from "@/components/PlacesToVisitGlobal.vue";
+export default {
+  name: "Itineraries",
+  props:['itineraryId'],
+  components: {
+    PlacesToVisitGlobal,
+    GoogleMaps,
+    NavBar
+  },
+  data() {
+    return {
+      selectedPlace: null
+    };
+  },
+  methods: {
+    handlePlaceSelection(place) {
+      this.selectedPlace = place;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.content-container {
+  display: flex;
+  width: 100vw; 
+  height: calc(100vh - 9vh); /* Since navbar occupies 9vh */
+}
+
+.places, .maps {
+  flex: 1;
+}
+
+.places {
+  overflow-y: auto;
+}
+
+#itineraries_tab {
+  background-color: aquamarine;
+}
+
+</style>
