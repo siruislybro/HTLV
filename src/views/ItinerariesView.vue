86548146a710ca3d470a888603a5e67a55546bb1
Tan Si Rui <e0564742@u.nus.edu>
2024-04-24 19:36:50 +0800

    feat: Implemented user permission checks for itineraries

--- src/views/ItinerariesView.vue
+++ src/views/ItinerariesView.vue
@@ -1,6 +1,6 @@
 <template>
   <NavBar />
-  <div class="content-container">
+  <div class="content-container" v-if="hasPermission">
     <div class="places">
       <PlacesToVisit
         @route-requested="handleRouteRequest"
@@ -16,12 +16,17 @@
       />
     </div>
   </div>
+  <div v-else>
+    <p class="error-message">{{ errorMessage }}</p>
+  </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
 import GoogleMaps from "../components/GoogleMaps.vue";
 import PlacesToVisit from "@/components/PlacesToVisit.vue";
+import { doc, setDoc, getDoc, addDoc, getFirestore, collection } from "firebase/firestore";
+
 export default {
   name: "Itineraries",
   props: ["itineraryId"],
@@ -34,12 +39,32 @@ export default {
     return {
       selectedPlace: null,
       destination: null,
+      hasPermission: true, // Indicates if the user has permission
+      errorMessage: '' // Store error messages
     };
   },
   methods: {
     // handlePlaceSelection(place) {
     //   this.selectedPlace = place;
     // }
+
+    async checkItineraryPermission() {
+      const userId = this.$store.getters['user/userUID'];
+      const itineraryRef = doc(getFirestore(), "users", userId, "itineraries", this.itineraryId);
+
+      try {
+        const docSnap = await getDoc(itineraryRef);
+        if (!docSnap.exists()) {
+          this.hasPermission = false;
+          this.errorMessage = "You do not have permission to view this itinerary.";
+          console.error(this.errorMessage);
+        }
+      } catch (error) {
+        console.error("Error checking permissions:", error);
+        this.errorMessage = "Failed to verify itinerary permissions.";
+      }
+    },
+
 
     handleRouteRequest(route) {
       this.$refs.googleMaps.displayRoute(
@@ -53,6 +78,9 @@ export default {
     handleDestinationUpdate(newDestination) {
       this.destination = newDestination;
     },
+  },
+  mounted() {
+    this.checkItineraryPermission();
   },
 };
 </script>
@@ -77,4 +105,12 @@ export default {
 #itineraries_tab {
   background-color: aquamarine;
 }
+
+.error-message{
+  font-size: 40px;
+  font-weight: 800;
+  text-align: center;
+  text-transform: capitalize;
+}
 </style>
+
